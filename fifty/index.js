160d5ebdc43cc06121e5213fd4246fd34d9f5c0b
omnisyle <omnisyle@gmail.com>
2019-05-01 01:37:17 -0400

    add logic to timer

--- fifty/index.js
+++ fifty/index.js
@@ -5,9 +5,42 @@ function Tile() {
 
 function Timer() {
   this.time = 0;
-  this.start = function() {};
-  this.stop = function() {};
-  this.reset = function() {};
+  this.formattedTime = "0";
+  this.timer = null;
+
+  this.update = function() {
+    var newSecond = this.time + 1;
+    var minutes = Math.floor(newSecond / 60);
+    var seconds = newSecond - minutes * 60;
+
+    var formattedMinutes = this.padLeft(minutes, "0", 2);
+    var formattedSeconds = this.padLeft(seconds, "0", 2);
+
+    this.time = newSecond;
+    this.formattedTime = minutes > 0
+      ? formattedMinutes + ":" + formattedSeconds
+      : seconds;
+    console.log(this.formattedTime);
+  };
+
+  this.padLeft = function(string, pad, length) {
+    return (new Array(length+1).join(pad)+string).slice(-length);
+  }
+
+  this.start = function() {
+    this.timer = setInterval(this.update.bind(this), 1000);
+  };
+
+  this.stop = function() {
+    clearInterval(this.timer);
+  };
+
+  this.reset = function() {
+    clearInterval(this.timer);
+    this.time = 0;
+    this.timer = null;
+    this.formattedTime = "0";
+  };
 }
 
 function Fifty() {
@@ -15,6 +48,14 @@ function Fifty() {
   this.render = function(element) {};
   this.isDone = function() {};
   this.completeGame = function() {};
+  this.init = init;
+}
+
+function init() {
+  var timer = new Timer();
+  timer.start();
 }
 
 var gameContainer = document.getElementById("fifty-container");
+var game = new Fifty();
+game.init();
